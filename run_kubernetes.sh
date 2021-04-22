dc80af171fdf49f93b3b9961c8626e3d3bc8b12c
mariamMazen <mariam.m.mazen@gmail.com>
2021-04-22 11:55:15 +0200

    review comments fix

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath="mariammazen/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment project4-submission --image=mariammazen/api:firsttry --port=80 
+kubectl create deployment project4-sub --image=mariammazen/api:firsttry --port=80 
 
 # Step 3:
 # List kubernetes pods
@@ -16,5 +16,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward project4-submission-55df679b55-lb7wc 8000:80
+kubectl port-forward project4-sub-779df7bb97-ss65h 8000:80
 
